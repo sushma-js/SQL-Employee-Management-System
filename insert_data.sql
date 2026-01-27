bd2dcfdd5ab0fa1e8e756ac8cb0f4fcf144c1a72
sushma-js <sushmajs1206@gmail.com>
2026-01-27 09:29:24 +0530

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO departments VALUES (1,'HR','Bangalore');
+INSERT INTO departments VALUES (2,'IT','Bangalore');
+INSERT INTO departments VALUES (3,'Finance','Mumbai');
+
+INSERT INTO employees VALUES (101,'Ravi','Manager',60000,1);
+INSERT INTO employees VALUES (102,'Anu','Developer',50000,2);
+INSERT INTO employees VALUES (103,'John','Tester',40000,2);
+INSERT INTO employees VALUES (104,'Meena','Clerk',30000,3);
